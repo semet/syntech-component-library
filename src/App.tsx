f5d62dd6591cc3ed4af9a5b38ae9ed73a118b426
Hamdani <hamdani@synergizetech.dev>
2025-11-18 09:42:29 +0800

    feat: add more more icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import CheckboxExamples from './examples/CheckboxExamples'
 import RadioExamples from './examples/RadioExamples'
 import SimpleFormExample from './examples/SimpleFormExample'
-import { BankManagementIcon } from './icons'
 
 export default function App() {
   return (
     <>
-      <BankManagementIcon />
       <SimpleFormExample />
       <CheckboxExamples />
       <RadioExamples />
