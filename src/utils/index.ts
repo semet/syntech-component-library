0b408ff6e3582b27b1b06ec0af1dc7579feb1336
Hamdani <hamdani@synergizetech.dev>
2025-11-19 22:24:17 +0800

    feat: export additional date formatting utilities from date-formatter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,8 @@
 export * from './color-helpers'
 export { type DateFormat, dateUtils } from './date-helpers'
+export {
+  formatDate,
+  type ExtendedDateFormat,
+  type Locale,
+  type TimeZone,
+} from './date-formatter'
